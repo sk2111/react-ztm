0dea2911414c6b57656dc3d4b4a904c9c6ce1f65
Sathishkumar Thangavel <sathish.thangavel@solitontech.local>
2020-10-08 16:46:49 +0530

    Updates

---
+++ precache-manifest.b452e1227d25d7bd654783ca52afad12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15410ebf77a39d8ec2ebd0dbb3f5311",
+    "url": "/react-ztm/index.html"
+  },
+  {
+    "revision": "c08af0d192e43b7789d7",
+    "url": "/react-ztm/static/css/main.a10d0e18.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/react-ztm/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ztm/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08af0d192e43b7789d7",
+    "url": "/react-ztm/static/js/main.b4df2b1b.chunk.js"
+  },
+  {
+    "revision": "936ce926957d62b34b5d",
+    "url": "/react-ztm/static/js/runtime-main.ba692c32.js"
+  }
+]);
