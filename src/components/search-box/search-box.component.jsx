f1060581a1fd556e50ea0d5da8e532287f75a938
Sathishkumar Thangavel <sathish.thangavel@solitontech.local>
2020-10-08 16:33:06 +0530

    Add : Style name for search component
    Signed-off-by: Sathishkumar Thangavel <sathish.thangavel@solitontech.local>

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './search-box.styles.css';
 
 export const SearchBox = ({placeHolder,searchField,onInputChange}) => {
     return (
-        <input type="search" placeholder={placeHolder} value={searchField} onChange={onInputChange}></input>
+        <input className="search" type="search" placeholder={placeHolder} value={searchField} onChange={onInputChange}></input>
     );
 }
